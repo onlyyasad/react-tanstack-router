35223c7299cb3b366c4f4bb86dd6e7287a382eb9
hafij <hafij.alasad23@gmail.com>
2025-02-15 23:31:49 +0600

    refactor: sidebar menu re-organized

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -56,6 +56,10 @@ const data = {
       isActive: true,
       items: [
         {
+          title: "Home",
+          url: "/",
+        },
+        {
           title: "Overview",
           url: "/overview",
         },
@@ -68,89 +72,14 @@ const data = {
           url: "#",
         },
       ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
+    }
   ],
   projects: [
     {
       name: "Login",
       url: "/login",
       icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
+    }
   ],
 }
 
--- src/routes/_layout/index.lazy.tsx
+++ src/routes/_layout/index.lazy.tsx
@@ -1,19 +1,46 @@
-import { Button } from '@/components/ui/button'
-import { createLazyFileRoute, Link } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { createLazyFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/_layout/')({
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export const Route = createLazyFileRoute("/_layout/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <div>
-      Hello "/"!
+      <div>
+        <Carousel className="w-full max-w-xs">
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
       <Button asChild>
-        <Link to='/teachers'>
-        Teachers
-        </Link>
+        <Link to="/teachers">Teachers</Link>
       </Button>
     </div>
-  )
+  );
 }
